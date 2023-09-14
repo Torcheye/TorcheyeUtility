ed10bbf940a2d376a94d6d5e07ec434e8024a9ea
King Zhou <zhouyimingking@outlook.com>
2023-09-14 00:34:16 -0700

    Add AudioManager, modify ApplicationManager and ReadMe

--- ApplicationManager.cs
+++ ApplicationManager.cs
@@ -1,23 +1,34 @@
 namespace TorcheyeUtility
 {
-    using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.SceneManagement;
     
     public class ApplicationManager : MonoBehaviour
     {
         [Header("Application Logic")]
+        [Tooltip("Reload current scene")]
         [SerializeField] private KeyCode restartGame = KeyCode.R;
         [SerializeField] private KeyCode exitGame = KeyCode.Escape;
         
         [Header("Scene Control")]
+        [Tooltip("Back to the first scene if exceeds scene count")]
         [SerializeField] private KeyCode nextScene = KeyCode.RightBracket;
+        [Tooltip("Back to the last scene if less than 0")]
         [SerializeField] private KeyCode previousScene = KeyCode.LeftBracket;
 
         [Header("Audio Volume Control")] 
         [SerializeField] private KeyCode increaseVolume = KeyCode.Equals;
         [SerializeField] private KeyCode decreaseVolume = KeyCode.Minus;
-        [SerializeField] private List<AudioSource> audioSources;
+        [SerializeField] private bool autoDetectAllAudioSource;
+        [SerializeField] private AudioSource[] audioSources;
+        [SerializeField] private float increaseMultiplier = 1.5f;
+        [SerializeField] private float decreaseMultiplier = 0.75f;
+
+        private void Awake()
+        {
+            if (autoDetectAllAudioSource)
+                audioSources = FindObjectsOfType<AudioSource>();
+        }
         
         private void Update()
         {
@@ -32,16 +43,13 @@ namespace TorcheyeUtility
                 SceneManager.LoadScene((activeScene + 1) % totalScene);
             if (Input.GetKeyDown(previousScene))
                 SceneManager.LoadScene((activeScene - 1) % totalScene);
+
             if (Input.GetKeyDown(increaseVolume))
                 foreach (AudioSource audioSource in audioSources)
-                {
-                    audioSource.volume *= 1.5f;
-                }
+                    audioSource.volume *= increaseMultiplier;
             if (Input.GetKeyDown(decreaseVolume))
                 foreach (AudioSource audioSource in audioSources)
-                {
-                    audioSource.volume *= 0.75f;
-                }
+                    audioSource.volume *= decreaseMultiplier;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TorcheyeUtility
-Unity utility scripts
+Unity utility scripts created for simple game managements.
+
+- `ApplicationManager`
+  - Restart, Exit, Scene navigation, Volume nontrol
+  - Custom key mapping
+
+- `AudioManager`
+  - Two `AudioSource` for music and sound effects
+  - 
